98e4ddb0dec6c0ab80b128c1b3efdb0235155a58
UchuNoKen <xen.o.fite@gmail.com>
2019-03-20 10:27:52 -0400

    shallow copy an array lesson

---
+++ 1_arrays/006_shallowCopyAnArray.js
@@ -0,0 +1,9 @@
+// Shallow copy an array
+let luckyNums = [7, 1, 3, 75, 13, 31, 21, 10];
+let otherLuckyNums = luckyNums;
+
+// or
+
+let luckyNums = [7, 1, 3, 75, 13, 31, 21, 10];
+let otherLuckyNums = [];
+otherLuckyNums.concat(luckyNums);
