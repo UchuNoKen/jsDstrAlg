80d9a387d17b6297c84d251a25c040fcf4d6372b
UchuNoKen <xen.o.fite@gmail.com>
2019-03-20 16:32:23 -0400

    concat lesson

---
+++ 001_arrays/002_accessorFunctions/004_concat.js
@@ -0,0 +1,16 @@
+// concat()
+
+let workers = ["Mike", "Danny", "Jennifer"];
+let itBosses = ["Raymond", "Cynthia", "Bryan"];
+
+let itDiv = workers.concat(itBosses);
+
+let hr = ["Barry", "Megan", "Walt", "Nicole"];
+let accounting = ["Larry", "Kevin", "Thomas", "Shania"];
+let marketing = ["Paul", "Will", "Forrest", "David"];
+
+let employees = workers
+  .concat(itBosses)
+  .concat(hr)
+  .concat(accounting)
+  .concat(marketing);
