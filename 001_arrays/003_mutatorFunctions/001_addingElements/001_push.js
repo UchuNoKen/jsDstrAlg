2948df52cd4ede70a28ce82b6f9f358f1b23c101
UchuNoKen <xen.o.fite@gmail.com>
2019-03-20 17:20:35 -0400

    push lesson

---
+++ 001_arrays/003_mutatorFunctions/001_addingElements/001_push.js
@@ -0,0 +1,10 @@
+// push()
+
+var nums = [1, 2, 3, 4, 5];
+console.log(nums);
+nums.push(6); // push 6 into the array
+console.log(nums);
+
+var nums = [1, 2, 3, 4, 5];
+nums[nums.length] = 6; // extend array with length property
+console.log(nums);
