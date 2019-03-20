28129dedde5ed50bacfb4613f28bc533596a2eea
UchuNoKen <xen.o.fite@gmail.com>
2019-03-20 16:15:10 -0400

    toString lesson

---
+++ 001_arrays/002_accessorFunctions/003_toString.js
@@ -0,0 +1,5 @@
+// toString()
+
+let arrOfNames = ["David", "Mike", "Cynthia", "Raymond", "Clayton", "Jennifer", "Mike"];
+let nameJoin = arrOfNames.join("");
+let nameToString = arrOfNames.toString();
